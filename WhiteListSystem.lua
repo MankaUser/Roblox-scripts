bd2fee3954965185f4dc6728b4cb29d378fa3fde
MankaUser <122668564+MankaUser@users.noreply.github.com>
2023-02-26 12:57:23 +0200

    WhiteListSystem
    whitelist system for my vape config

---
+++ WhiteListSystem.lua
@@ -0,0 +1,21 @@
+if game.Player.LocalPlayer.UserID == 4278366218 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/5a8e3a9e9ffca08983dcf948c8d00c781c34a4d8/NewMainScript.lua"))()
+writefile("vape/MankaEditionVersion.txt", "Version: 1.0")
+  wait(1)
+makefolder("vape/VapeMankaAdditions")
+makefolder("vape/VapeMankaAdditions/assets")
+makefolder("vape/VapeMankaAdditions/CustomModules")
+makefolder("vape/VapeMankaAdditions/VapeAdditions")
+writefile("vape/VapeMankaAdditions/MankaEditionVersion.txt", "Version: 1.0")
+  end
+
+if game.Player.LocalPlayer.UserID == 4374006561 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/5a8e3a9e9ffca08983dcf948c8d00c781c34a4d8/NewMainScript.lua"))()
+writefile("vape/MankaEditionVersion.txt", "Version: 1.0")
+  wait(1)
+makefolder("vape/VapeMankaAdditions")
+makefolder("vape/VapeMankaAdditions/assets")
+makefolder("vape/VapeMankaAdditions/CustomModules")
+makefolder("vape/VapeMankaAdditions/VapeAdditions")
+writefile("vape/VapeMankaAdditions/MankaEditionVersion.txt", "Version: 1.0")
+  end
